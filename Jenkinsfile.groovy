1b56ea2be389008c83ea2814b7412df67661e508
Chu Tingting <chuttin@163.com>
2021-08-30 16:44:37 +0800

    add jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,11 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('pull code') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[credentialsId: '1903b0e9-1b40-46c8-8579-147f15703c21', url: 'https://github.com/chuttin/jenkinsTest.git']]])
+            }
+        }
+    }
+}
